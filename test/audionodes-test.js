9e17cc797397a184b7997ba9d9409fd08ef20785
Sebastien Piquemal <sebpiq@gmail.com>
2013-07-03 19:41:22 +0400

    soundfile loop

--- test/audionodes-test.js
+++ test/audionodes-test.js
@@ -13,7 +13,7 @@ describe('SoundFile', function() {
     })}
   
     it('should read basic wav files', function(done) {
-      var soundfile = new audionodes.SoundFile(__dirname + '/sounds/steps.wav', 4410 * 2)
+      var soundfile = new audionodes.SoundFile(__dirname + '/sounds/steps.wav')
         , blocks = []
         
       soundfile.on('error', function(err) { console.error(err) })
@@ -61,6 +61,56 @@ describe('SoundFile', function() {
         
       })
     })
+
+    it('should loop through a file if loop=true', function(done) {
+      var soundfile = new audionodes.SoundFile(__dirname + '/sounds/stepsShort.wav', {loop: true})
+        , blocks = []
+        
+      soundfile.on('error', function(err) { console.error(err) })
+      soundfile.on('ready', function() {
+        var sampleCount = 0
+        async.whilst(
+          function() { return blocks.length < 22 },
+          function(next) {
+            soundfile.read(44100 * 0.02, function(err, block) {
+              if (err) next(err)
+              assert.equal(block.length, 44100 * 0.02)
+              blocks.push(block.slice(100, 105)) // Pd is not exact with timing
+              next()
+            })
+          },
+          function(err) {
+            if (err) throw err
+            assert.deepEqual(round(blocks[0]), [-1, -1, -1, -1, -1])
+            assert.deepEqual(round(blocks[1]), [-0.8, -0.8, -0.8, -0.8, -0.8])
+            assert.deepEqual(round(blocks[2]), [-0.6, -0.6, -0.6, -0.6, -0.6])
+            assert.deepEqual(round(blocks[3]), [-0.4, -0.4, -0.4, -0.4, -0.4])
+            assert.deepEqual(round(blocks[4]), [-0.2, -0.2, -0.2, -0.2, -0.2])
+            assert.deepEqual(round(blocks[5]), [0, 0, 0, 0, 0])
+            assert.deepEqual(round(blocks[6]), [0.2, 0.2, 0.2, 0.2, 0.2])
+            assert.deepEqual(round(blocks[7]), [0.4, 0.4, 0.4, 0.4, 0.4])
+            assert.deepEqual(round(blocks[8]), [0.6, 0.6, 0.6, 0.6, 0.6])
+            assert.deepEqual(round(blocks[9]), [0.8, 0.8, 0.8, 0.8, 0.8])
+            assert.deepEqual(round(blocks[10]), [-1, -1, -1, -1, -1])
+            assert.deepEqual(round(blocks[11]), [-0.8, -0.8, -0.8, -0.8, -0.8])
+            assert.deepEqual(round(blocks[12]), [-0.6, -0.6, -0.6, -0.6, -0.6])
+            assert.deepEqual(round(blocks[13]), [-0.4, -0.4, -0.4, -0.4, -0.4])
+            assert.deepEqual(round(blocks[14]), [-0.2, -0.2, -0.2, -0.2, -0.2])
+            assert.deepEqual(round(blocks[15]), [0, 0, 0, 0, 0])
+            assert.deepEqual(round(blocks[16]), [0.2, 0.2, 0.2, 0.2, 0.2])
+            assert.deepEqual(round(blocks[17]), [0.4, 0.4, 0.4, 0.4, 0.4])
+            assert.deepEqual(round(blocks[18]), [0.6, 0.6, 0.6, 0.6, 0.6])
+            assert.deepEqual(round(blocks[19]), [0.8, 0.8, 0.8, 0.8, 0.8])
+            assert.deepEqual(round(blocks[20]), [-1, -1, -1, -1, -1])
+            assert.deepEqual(round(blocks[21]), [-0.8, -0.8, -0.8, -0.8, -0.8])
+            assert.equal(soundfile.closed, false)
+            done()
+          }
+        )
+        
+        
+      })
+    })       
   
   })
 
