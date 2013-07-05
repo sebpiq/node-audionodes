8d42a1d93693573a636f31180a7495fb686f7424
Sebastien Piquemal <sebpiq@gmail.com>
2013-07-05 13:40:40 +0400

    refactor for stereo/multi-channel support

--- test/audionodes-test.js
+++ test/audionodes-test.js
@@ -12,12 +12,16 @@ describe('SoundFile', function() {
       return Math.round(val * Math.pow(10, dec)) / Math.pow(10, dec)
     })}
   
-    it('should read basic wav files', function(done) {
-      var soundfile = new audionodes.SoundFile(__dirname + '/sounds/steps.wav')
+    it('should read mono 16-bits wav files', function(done) {
+      var soundfile = new audionodes.SoundFile(__dirname + '/sounds/steps-mono-16b-44khz.wav')
         , blocks = []
         
       soundfile.on('error', function(err) { console.error(err) })
       soundfile.on('ready', function() {
+        assert.equal(soundfile.format.bitDepth, 16)
+        assert.equal(soundfile.format.channels, 1)
+        assert.equal(soundfile.format.sampleRate, 44100)
+
         var sampleCount = 0
         async.whilst(
           function() { return blocks.length < 22 },
@@ -63,11 +67,15 @@ describe('SoundFile', function() {
     })
 
     it('should loop through a file if loop=true', function(done) {
-      var soundfile = new audionodes.SoundFile(__dirname + '/sounds/stepsShort.wav', {loop: true})
+      var soundfile = new audionodes.SoundFile(__dirname + '/sounds/stepsShort-mono-16b-44khz.wav', {loop: true})
         , blocks = []
         
       soundfile.on('error', function(err) { console.error(err) })
       soundfile.on('ready', function() {
+        assert.equal(soundfile.format.bitDepth, 16)
+        assert.equal(soundfile.format.channels, 1)
+        assert.equal(soundfile.format.sampleRate, 44100)
+
         var sampleCount = 0
         async.whilst(
           function() { return blocks.length < 22 },
@@ -112,12 +120,16 @@ describe('SoundFile', function() {
       })
     })
 
-    it('should read mp3 files', function(done) {
-      var soundfile = new audionodes.SoundFile(__dirname + '/sounds/steps.mp3')
+    it('should read mono mp3 files', function(done) {
+      var soundfile = new audionodes.SoundFile(__dirname + '/sounds/steps-mono-44khz.mp3')
         , blocks = []
         
       soundfile.on('error', function(err) { console.error(err) })
       soundfile.on('ready', function() {
+        assert.equal(soundfile.format.bitDepth, 16)
+        assert.equal(soundfile.format.channels, 1)
+        assert.equal(soundfile.format.sampleRate, 44100)
+
         var sampleCount = 0
         async.whilst(
           function() { return blocks.length < 22 },
@@ -161,7 +173,61 @@ describe('SoundFile', function() {
         
       })
     })
-  
+
+    /*it('should read stereo 16-bits wav files', function(done) {
+      var soundfile = new audionodes.SoundFile(__dirname + '/sounds/steps.wav')
+        , blocks = []
+        
+      soundfile.on('error', function(err) { console.error(err) })
+      soundfile.on('ready', function() {
+        assert.equal(soundfile.format.bitDepth, 16)
+        assert.equal(soundfile.format.channels, 1)
+        assert.equal(soundfile.format.sampleRate, 44100)
+
+        var sampleCount = 0
+        async.whilst(
+          function() { return blocks.length < 22 },
+          function(next) {
+            soundfile.read(4410, function(err, block) {
+              if (err) next(err)
+              assert.equal(block.length, 4410)
+              blocks.push(block.slice(100, 105)) // Pd is not exact with timing
+              next()
+            })
+          },
+          function(err) {
+            if (err) throw err
+            assert.deepEqual(round(blocks[0]), [-1, -1, -1, -1, -1])
+            assert.deepEqual(round(blocks[1]), [-0.9, -0.9, -0.9, -0.9, -0.9])
+            assert.deepEqual(round(blocks[2]), [-0.8, -0.8, -0.8, -0.8, -0.8])
+            assert.deepEqual(round(blocks[3]), [-0.7, -0.7, -0.7, -0.7, -0.7])
+            assert.deepEqual(round(blocks[4]), [-0.6, -0.6, -0.6, -0.6, -0.6])
+            assert.deepEqual(round(blocks[5]), [-0.5, -0.5, -0.5, -0.5, -0.5])
+            assert.deepEqual(round(blocks[6]), [-0.4, -0.4, -0.4, -0.4, -0.4])
+            assert.deepEqual(round(blocks[7]), [-0.3, -0.3, -0.3, -0.3, -0.3])
+            assert.deepEqual(round(blocks[8]), [-0.2, -0.2, -0.2, -0.2, -0.2])
+            assert.deepEqual(round(blocks[9]), [-0.1, -0.1, -0.1, -0.1, -0.1])
+            assert.deepEqual(round(blocks[10]), [0, 0, 0, 0, 0])
+            assert.deepEqual(round(blocks[11]), [0.1, 0.1, 0.1, 0.1, 0.1])
+            assert.deepEqual(round(blocks[12]), [0.2, 0.2, 0.2, 0.2, 0.2])
+            assert.deepEqual(round(blocks[13]), [0.3, 0.3, 0.3, 0.3, 0.3])
+            assert.deepEqual(round(blocks[14]), [0.4, 0.4, 0.4, 0.4, 0.4])
+            assert.deepEqual(round(blocks[15]), [0.5, 0.5, 0.5, 0.5, 0.5])
+            assert.deepEqual(round(blocks[16]), [0.6, 0.6, 0.6, 0.6, 0.6])
+            assert.deepEqual(round(blocks[17]), [0.7, 0.7, 0.7, 0.7, 0.7])
+            assert.deepEqual(round(blocks[18]), [0.8, 0.8, 0.8, 0.8, 0.8])
+            assert.deepEqual(round(blocks[19]), [0.9, 0.9, 0.9, 0.9, 0.9])
+            assert.deepEqual(round(blocks[20]), [1, 1, 1, 1, 1])
+            assert.deepEqual(round(blocks[21]), [0, 0, 0, 0, 0])
+            assert.equal(soundfile.closed, true)
+            done()
+          }
+        )
+        
+        
+      })
+    })*/
+
   })
 
 })
--- test/basenodes-test.js
+++ test/basenodes-test.js
@@ -9,65 +9,76 @@ describe('SourceNode', function() {
     
     var MySourceNode = audionodes.SourceNode.extend({
     
-      init: function() {
+      init: function(channels) {
         this.counter = -1
+        this.channels = channels || 1
       },
     
       getBlock: function(next) {
         this.counter++
         if (this.counter < 3) {
-          next(null, _.range(this.counter * 10, (this.counter + 1) * 10))
-        } else next(null, [])
+          if (this.channels === 1) {
+            next(null, [_.range(this.counter * 10, (this.counter + 1) * 10)])
+          } else {
+            next(null, [
+              _.range(this.counter * 10, (this.counter + 1) * 10),
+              _.range(this.counter * 20, (this.counter + 1) * 20, 2)
+            ])
+          }
+        } else {
+          this.close()
+          this.getBlock(next)
+        }
       }
       
     })
     
     it('should read the right amount of data and buffer the surplus', function(done) {
       var node = new MySourceNode()
-      assert.deepEqual(node._buffer, [])
+      assert.deepEqual(node._buffers, [[]])
       
       async.waterfall([
         function(next) {
           node.read(5, next)
         },
         function(block, next) {
-          assert.deepEqual(block, [0, 1, 2, 3, 4])
-          assert.deepEqual(node._buffer, [5, 6, 7, 8, 9])
+          assert.deepEqual(block, [[0, 1, 2, 3, 4]])
+          assert.deepEqual(node._buffers, [[5, 6, 7, 8, 9]])
           node.read(5, next)
         },
         function(block, next) {
-          assert.deepEqual(block, [5, 6, 7, 8, 9])
-          assert.deepEqual(node._buffer, [])
+          assert.deepEqual(block, [[5, 6, 7, 8, 9]])
+          assert.deepEqual(node._buffers, [[]])
           node.read(12, next)
         },
         function(block) {
-          assert.deepEqual(block, [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
-          assert.deepEqual(node._buffer, [22, 23, 24, 25, 26, 27, 28, 29])
+          assert.deepEqual(block, [[10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]])
+          assert.deepEqual(node._buffers, [[22, 23, 24, 25, 26, 27, 28, 29]])
           done()
         }
       ])
     })
     
     it('should read the right amount of data and pad the missing data', function(done) {
-      var node = new MySourceNode()
-      assert.deepEqual(node._buffer, [])
+      var node = new MySourceNode(2)
+      assert.deepEqual(node._buffers, [[], []])
       
       async.waterfall([
         function(next) {
           node.read(25, next)
         },
         function(block, next) {
-          assert.deepEqual(node._buffer, [25, 26, 27, 28, 29])
+          assert.deepEqual(node._buffers, [[25, 26, 27, 28, 29], [50, 52, 54, 56, 58]])
           node.read(10, next)
         },
         function(block, next) {
-          assert.deepEqual(block, [25, 26, 27, 28, 29, 0, 0, 0, 0, 0])
-          assert.deepEqual(node._buffer, [])
+          assert.deepEqual(block, [[25, 26, 27, 28, 29, 0, 0, 0, 0, 0], [50, 52, 54, 56, 58, 0, 0, 0, 0, 0]])
+          assert.deepEqual(node._buffers, [[], []])
           node.read(5, next)
         },
         function(block) {
-          assert.deepEqual(block, [0, 0, 0, 0, 0])
-          assert.deepEqual(node._buffer, [])
+          assert.deepEqual(block, [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])
+          assert.deepEqual(node._buffers, [[], []])
           done()
         }
       ])
